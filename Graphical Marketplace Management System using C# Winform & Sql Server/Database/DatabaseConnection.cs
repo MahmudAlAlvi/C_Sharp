8b8f19a46808f87243680508607435224190f065
Mahmud Al Alvi <168158713+MahmudAlAlvi@users.noreply.github.com>
2025-05-12 02:04:11 +0600

    Add files via upload

---
+++ Graphical Marketplace Management System using C# Winform & Sql Server/Database/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Online_MarketPlace.Database
+{
+    public abstract class DatabaseConnection
+    {
+        const string connectionString = @"Data Source=ALVI08\SQLEXPRESS;Initial Catalog=E-Shop;Integrated Security=True;Encrypt=False";
+        public static SqlConnection databaseConnect()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
+//ALVI08\SQLEXPRESS
