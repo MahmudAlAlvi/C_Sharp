23a21596678835b6948615b13a20eac0cd4e5480
Mahmud Al Alvi <168158713+MahmudAlAlvi@users.noreply.github.com>
2025-05-12 02:05:31 +0600

    Add files via upload

---
+++ Graphical Marketplace Management System using C# Winform & Sql Server/Forms/CustomerForm.cs
@@ -0,0 +1,77 @@
+﻿using Online_MarketPlace.User_Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Online_MarketPlace.Forms
+{
+    public partial class CustomerForm : Form
+    {
+        string username = LoginForm.lf.GetWelcomeUserData();
+        public CustomerForm()
+        {
+            InitializeComponent();
+
+            customer_menu_label.Text = $"Welcome, {username}\nType: Customer";
+
+            shopping1.BringToFront();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm.lf.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void shopBtn_Click(object sender, EventArgs e)
+        {
+            shopping1.BringToFront();
+        }
+
+        private void settingsBtn_Click(object sender, EventArgs e)
+        {
+            customerSetting1.BringToFront();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void customer_menu_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shopping1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sideBarPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void upperBarPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Graphical Marketplace Management System using C# Winform & Sql Server/Forms/OwnerForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Online_MarketPlace
+{
+    public partial class OwnerForm : Form
+    {
+        public OwnerForm()
+        {
+            InitializeComponent();
+            string sharedData = LoginForm.lf.GetWelcomeUserData();
+            owner_menu_label.Text = $"Hello, {sharedData}";
+            ownerDashboard1.Dock = DockStyle.Fill;
+            ownerDashboard1.BringToFront();
+        }
+
+        private void logoutBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm.lf.Show();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void dashboardBtn_Click(object sender, EventArgs e)
+        {
+            ownerDashboard1.BringToFront();
+        }
+
+        private void managementBtn_Click(object sender, EventArgs e)
+        {
+            ownerManagement1.BringToFront();
+        }
+
+        private void financeBtn_Click(object sender, EventArgs e)
+        {
+            ownerFinance1.BringToFront();
+        }
+
+        private void settingsBtn_Click(object sender, EventArgs e)
+        {
+            ownerSettings1.BringToFront();
+        }
+
+        private void OwnershipBtn_Click(object sender, EventArgs e)
+        {
+            ownership1.BringToFront();
+        }
+
+        private void ownerDashboard1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void upperBarPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void owner_pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void owner_menu_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sideBarPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
